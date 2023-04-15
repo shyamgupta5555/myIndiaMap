a9ef947701b95daf6c926c2d49b5994c2e5d0dc3
shyam <shyamgupta0214@gmail.com>
2023-04-15 19:30:16 +0530

    added

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
 const route = require("./route/route");
 const cors = require("cors");
-const multer = require("multer")
+const multer = require("multer");
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../build")));
 app.use(cors());
-app.use(multer().any())
+app.use(multer().any());
 mongoose
   .connect(
     "mongodb+srv://shyamgupta:.T!8NRrzf6FyMYc@cluster0.dbdyccj.mongodb.net/mapproject"
@@ -24,8 +25,7 @@ app.use("/", route);
 
 app.listen(5000, console.log("Application is running", 5000));
 
-app.get("*",(req,res)=>{
-const files = path.join(__dirname,"../build/index.html")
-res.sendFile(files)
-
-})
+app.get("*", (req, res) => {
+  const files = path.join(__dirname, "../build/index.html");
+  res.sendFile(files);
+});
