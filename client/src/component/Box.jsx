9213e45d7967b76182509c9e85e948047665891d
shyam <shyamgupta0214@gmail.com>
2023-04-23 21:51:42 +0530

    addedd

--- client/src/component/Box.jsx
+++ client/src/component/Box.jsx
@@ -14,7 +14,7 @@ export function Boxx() {
         setMemoriesImage(response.data.data);
       })
       .catch((error) => {
-        if (error.response.data.message == "jwt expired") {
+        if (error.response.data.message === "jwt expired") {
           navigate("/login");
         }
         console.log(error.message);
@@ -44,19 +44,3 @@ export function Boxx() {
     </div>
   );
 }
-
-//  {/* {memoriesImage.map(()=>{
-//       <p>{memoriesImage.content}</p>
-//     })} */}
-//       {/* <div className="card">
-//         <div className="upper-container">
-//           <div className="image-container">
-//             <img src="https://images.pexels.com/photos/15851976/pexels-photo-15851976.jpeg" alt=""  height="100px"  width="100px" />
-//           </div>
-//         </div>
-//         <div className="lower-container">
-//           <h3> your name</h3>
-//           <p>Hi, I am Manisha Singh. If you talk about my educational background, I completed my Bachelor of Engineering degree in 2006. After that, I changed my line of profession and got into writing for the English language always remained my first love.</p>
-
-//         </div>
-//       </div> */}
